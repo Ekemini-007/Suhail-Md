86c46f25ec1ef4832ea0d3fee2bd1778050e52fa
Ekemini-007 <153646685+Ekemini-007@users.noreply.github.com>
2024-08-07 16:11:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.DATABASE_URL = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
-global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
+global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VagVbj489inn4IaPif3z";
+global.website=process.env.GURL || "https://whatsapp.com/channel/0029VagVbj489inn4IaPif3z" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/eed7dbddb754226714c01.jpg" ; // SET LOGO FOR IMAGE 
 global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙺𝙷𝙴𝙼𝙸" 
 
